3a8fe433ce9d16cc6c9ad2c168e891926cd11cda
Leskelä Heikki Johannes <hleskel@gmail.com>
2025-08-03 11:44:04 +0300

    Trying to get 3.15 working

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
@@ -10,28 +12,28 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use(express.static('dist'))
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": "1"
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": "2"
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": "3"
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": "4"
-    }
-  ]
+// let persons = [
+//     {
+//       "name": "Arto Hellas",
+//       "number": "040-123456",
+//       "id": "1"
+//     },
+//     {
+//       "name": "Ada Lovelace",
+//       "number": "39-44-5323523",
+//       "id": "2"
+//     },
+//     {
+//       "name": "Dan Abramov",
+//       "number": "12-43-234345",
+//       "id": "3"
+//     },
+//     {
+//       "name": "Mary Poppendieck",
+//       "number": "39-23-6423122",
+//       "id": "4"
+//     }
+//   ]
 
 app.get('/info', (request, response) => {
     const personCount = persons.length
@@ -44,24 +46,30 @@ app.get('/info', (request, response) => {
 })  
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-   if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = request.params.id
+//   persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
+//   response.status(204).end()
+// })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -71,28 +79,40 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number missing!' 
-    })
-  }
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({ 
+  //     error: 'name or number missing!' 
+  //   })
+  // }
 
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name already exists!' 
-    }) 
-  }
+  // if (persons.some(person => person.name === body.name)) {
+  //   return response.status(400).json({ 
+  //     error: 'name already exists!' 
+  //   }) 
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: generateId(),
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
-  persons = persons.concat(person)
+  next(error)
+}
 
-  response.json(person)
-})
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ osa3/puhelinluettelon_backend/models/person.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+// const password = process.argv[2]
+// const person = process.argv[3]
+// const number = process.argv[4]
+
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number
+})
+
+// const Person = mongoose.model('Person', personSchema)
+
+
+    // Person 
+    //     .find({})
+    //     .then(result => {
+    //         result.forEach(person => {
+    //         console.log(person)
+    //         })
+    //         mongoose.connection.close()
+    //     })
+
+// if (process.argv.length === 5) {
+//     const newPerson = new Person({
+//         name: person,
+//         number: number,
+//         id: String(1 + Math.floor(Math.random()*100000))
+//     })
+
+//     newPerson.save().then(result => {
+//         console.log('person saved to phonebook!')
+//         mongoose.connection.close()
+//         })
+// }
+
+// if (process.argv.length === 3) {
+//     Person 
+//         .find({})
+//         .then(result => {
+//             result.forEach(person => {
+//             console.log(person)
+//             })
+//             mongoose.connection.close()
+//         })
+// }
+
+module.exports = mongoose.model('Person', personSchema)
