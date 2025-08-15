f7162fedc49ed8782e5effa22bfd1157aa90203e
Leskelä Heikki Johannes <hleskel@gmail.com>
2025-08-15 13:53:20 +0300

    blogs controller works. blog.js model added, but not used yet

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,35 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const { info, error } = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
+app.use('/api/blogs', blogsRouter)
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 app.listen(config.PORT, () => {
   info(`Server running on port ${config.PORT}`)
