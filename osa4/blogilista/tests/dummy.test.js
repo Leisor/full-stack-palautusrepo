9cc0603bfcceb3e8097b2b8d2a101d7fd0fa59b9
Leskelä Heikki Johannes <hleskel@gmail.com>
2025-08-16 08:00:18 +0300

    stop tracking .env

--- osa4/blogilista/tests/dummy.test.js
+++ osa4/blogilista/tests/dummy.test.js
@@ -60,7 +60,7 @@ describe('total likes', () => {
 
 })
 
-describe('most likes', () => {
+describe('most liked blog', () => {
   const listWithOneBlog = [
     {
       _id: '5a422aa71b54a676234d17f8',
@@ -72,9 +72,9 @@ describe('most likes', () => {
     }
   ]
 
-  test('when list has only one blog returns the blog title', () => {
+  test('when list has only one blog returns that blog', () => {
     const result = listHelper.favoriteBlog(listWithOneBlog)
-    assert.strictEqual(result, listWithOneBlog[0])
+    assert.deepStrictEqual(result, listWithOneBlog[0])
   })
 
     const listWithThreeBlogs = [
@@ -104,9 +104,119 @@ describe('most likes', () => {
         }
     ]
 
-    test('most liked blog title from a list of three blogs', () => {
+    test('most liked blog from a list of three blogs', () => {
     const result = listHelper.favoriteBlog(listWithThreeBlogs)
-    assert.strictEqual(result, listWithThreeBlogs[2])
+    assert.deepStrictEqual(result, listWithThreeBlogs[2])
   })
 
+})
+
+describe('most blogs', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test("when list has only one blog returns that blog's auther and blog count (1)", () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      author: listWithOneBlog[0].author,
+      blogs: 1 })
+  })
+
+    const listWithThreeBlogs = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        }
+    ]
+
+    test('most productive auther and amount of blogs from a list of three blogs', () => {
+    const result = listHelper.mostBlogs(listWithThreeBlogs)
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      blogs: 2 })
+  
+  })
+})
+
+describe('most liked blogger', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test("when list has only one blog returns that blog's author and like count", () => {
+    const result = listHelper.mostLikes(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      author: listWithOneBlog[0].author,
+      likes: listWithOneBlog[0].likes })
+  })
+
+    const listWithThreeBlogs = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        }
+    ]
+
+    test('most liked author and amount of likes from a list of three blogs', () => {
+    const result = listHelper.mostLikes(listWithThreeBlogs)
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      likes: 17 })
+  
+  })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -11,8 +11,8 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
-    mostLikes = 0
-    blogWithMostLikes = ""
+    let mostLikes = 0
+    let blogWithMostLikes = ""
     blogs.forEach(blog => {
         if (blog.likes > mostLikes) {
             mostLikes = blog.likes
@@ -22,8 +22,76 @@ const favoriteBlog = (blogs) => {
     return blogWithMostLikes
 }
 
+const mostBlogs = (blogs) => {
+    let mostProductiveAuthor = ""
+    let mostProductiveAuthorBlogs = 0
+    const authorsAndBlogs = []
+
+    blogs.forEach(blog => {
+        const index = authorsAndBlogs.findIndex(a => a.author === blog.author)
+        if (index !== -1) {
+            authorsAndBlogs[index].blogs += 1
+        }
+        else {
+            authorsAndBlogs.push({
+                author: blog.author,
+                blogs: 1
+            })
+        }
+            
+    })
+
+    authorsAndBlogs.forEach(aAndB => {
+        if (aAndB.blogs > mostProductiveAuthorBlogs) {
+            mostProductiveAuthorBlogs = aAndB.blogs
+            mostProductiveAuthor = aAndB.author
+        }
+    })
+
+    return {
+        author: mostProductiveAuthor,
+        blogs: mostProductiveAuthorBlogs
+    }
+
+}
+
+const mostLikes = (blogs) => {
+    let mostPopularAuthor = ""
+    let mostPopularAuthorLikes = 0
+    const authorsAndLikes = []
+
+    blogs.forEach(blog => {
+        const index = authorsAndLikes.findIndex(a => a.author === blog.author)
+        if (index !== -1) {
+            authorsAndLikes[index].likes += blog.likes
+        }
+        else {
+            authorsAndLikes.push({
+                author: blog.author,
+                likes: blog.likes
+            })
+        }
+            
+    })
+
+    authorsAndLikes.forEach(aAndL => {
+        if (aAndL.likes > mostPopularAuthorLikes) {
+            mostPopularAuthorLikes = aAndL.likes
+            mostPopularAuthor = aAndL.author
+        }
+    })
+
+    return {
+        author: mostPopularAuthor,
+        likes: mostPopularAuthorLikes
+    }
+
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
