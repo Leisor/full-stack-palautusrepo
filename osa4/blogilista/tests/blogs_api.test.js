b6e32ae55db1e0f085029a422599552e6efda9bb
Leskelä Heikki Johannes <hleskel@gmail.com>
2025-08-16 14:10:45 +0300

    tehtävä 4.9 id testi lisätty, ohjelman toiminnallisuus lisäämättä

--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -1,45 +1,58 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
-test.only('blogs are returned as json', async () => {
+const listWithThreeBlogs = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        }
+    ]
+
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
+test('blog identifier must be id, not _id', async () => {
+    const response = await api
+      .get('api/blogs/a422b3a1b54a676234d17f9')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blog = response.body
+
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+        
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
-
-
-// const listWithThreeBlogs = [
-//         {
-//             _id: '5a422a851b54a676234d17f7',
-//             title: 'React patterns',
-//             author: 'Michael Chan',
-//             url: 'https://reactpatterns.com/',
-//             likes: 7,
-//             __v: 0
-//         },
-//         {
-//             _id: '5a422aa71b54a676234d17f8',
-//             title: 'Go To Statement Considered Harmful',
-//             author: 'Edsger W. Dijkstra',
-//             url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-//             likes: 5,
-//             __v: 0
-//         },
-//         {
-//             _id: '5a422b3a1b54a676234d17f9',
-//             title: 'Canonical string reduction',
-//             author: 'Edsger W. Dijkstra',
-//             url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-//             likes: 12,
-//             __v: 0
-//         }
-//     ]
