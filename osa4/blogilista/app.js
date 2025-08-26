7b29e1ec2a127dac65c19c3a2db6fe2806343b22
Heikki Leskelä <hleskel@gmail.com>
2025-08-26 13:01:41 +0300

    Tehtävä 4.20 valmis mutta ei testattu

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 //const { info, error } = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+
+
 
 const app = express()
 
@@ -13,6 +16,7 @@ const app = express()
 mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -14,14 +14,6 @@ blogsRouter.get('/:id', async (request, response) => {
 
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   const { title, url } = request.body
 
@@ -29,7 +21,7 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'title or url missing'})
   }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
