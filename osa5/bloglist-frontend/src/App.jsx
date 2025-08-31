fbb028b3777d378c056436acb7a4750ba87ee0d2
Leskelä Heikki Johannes <hleskel@gmail.com>
2025-08-31 07:39:28 +0300

    Tehtävä 5.4 valmis

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -3,6 +3,20 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={type === 'success'
+      ? 'success' 
+      : 'error'
+    }>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -10,6 +24,8 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [messageType, setMessageType] = useState('error')
+  const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -41,6 +57,7 @@ const App = () => {
       setPassword('')
     } catch {
       setErrorMessage('wrong credentials')
+      setMessageType('error')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -57,7 +74,7 @@ const App = () => {
     return (
       <div>
         <h2>Log in to application</h2>
-        {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+        <Notification message={errorMessage} type={messageType} />
         <form onSubmit={handleLogin}>
         <div>
           <label>
@@ -85,9 +102,31 @@ const App = () => {
     )
   }
 
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const createdBlog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(createdBlog))
+      setErrorMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
+      setMessageType('success')
+      setNewBlog({ title: '', author: '', url: '' })
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    } catch {
+      setErrorMessage('Blog not created!')
+      setMessageType('error')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={errorMessage} type={messageType} />
       <p>
         {user.name} logged in
         <button onClick={handleLogout}>logout</button>
@@ -95,6 +134,41 @@ const App = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      <h2>create new</h2>
+      <form onSubmit={handleNewBlog}>
+        <div>
+          <label>
+            title
+            <input
+              type="text"
+              value={newBlog.title}
+              onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            author
+            <input
+              type="text"
+              value={newBlog.author}
+              onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            url
+            <input
+              type="text"
+              value={newBlog.url}
+              onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })}
+            />
+          </label>
+        </div>
+        <button type="submit">create</button>
+      </form>
+
     </div>
   )
 }
